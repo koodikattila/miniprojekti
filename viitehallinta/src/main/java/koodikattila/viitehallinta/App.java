6e6413f2d83525737bc39da7332aecd4252c0f8a
WaDelma <delma@wa>
2014-03-28 16:07:00 +0200

    .gitignore lisätty ja perus Maven projecti

---
+++ viitehallinta/src/main/java/koodikattila/viitehallinta/App.java
@@ -0,0 +1,13 @@
+package koodikattila.viitehallinta;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
